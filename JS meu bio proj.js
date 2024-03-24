12fdfd49ba93295fbcab71a36c7941455e79ae44
Guilhermemeins <138955928+Guilhermemeins@users.noreply.github.com>
2024-03-23 21:28:23 -0400

    Add files via upload

---
+++ JS meu bio proj.js
@@ -0,0 +1,5 @@
+document.getElementById("botão certo"). addEventListener("click", function() 
+{alert("PARABÉNS botão certo!")});
+
+document.getElementById("botão errado"). addEventListener("click", function()
+{alert("BOTÃO ERRADO!")}); 
